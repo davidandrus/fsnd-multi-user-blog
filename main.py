09a6db65939ca9fa588637690f15c65c222497d0
davidandrus <davidandrus.com@gmail.com>
2017-02-02 04:52:34 -0800

    feat: adding jinja

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+"""
+Start App Engine Routes
+"""
+
 import webapp2
+import jinja2
+from os import path
+
+
+JINJA_TEMPLATE_PATH = path.join(path.dirname(__file__), 'templates')
+JINJA_ENV = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(JINJA_TEMPLATE_PATH),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template_values = {
+            'title': 'It Works!'
+        }
+        template = JINJA_ENV.get_template('index.html')
+        self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
