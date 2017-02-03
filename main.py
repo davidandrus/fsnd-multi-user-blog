6615b4eb9ad800d4c6ada156f1b17b151f3ba796
davidandrus <davidandrus.com@gmail.com>
2017-02-02 23:27:56 -0800

    feat: add base template, static assets dir, bootstrap, style registration page

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import webapp2
 import jinja2
 from os import path
 
-
 JINJA_TEMPLATE_PATH = path.join(path.dirname(__file__), 'templates')
 JINJA_ENV = jinja2.Environment(
     loader=jinja2.FileSystemLoader(JINJA_TEMPLATE_PATH),
@@ -18,9 +17,16 @@ class MainHandler(webapp2.RequestHandler):
         template_values = {
             'title': 'It Works!'
         }
-        template = JINJA_ENV.get_template('index.html')
+        template = JINJA_ENV.get_template('index.html.j2')
+        self.response.write(template.render(template_values))
+
+class RegisterHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENV.get_template('register.html.j2')
+        template_values = {}
         self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/register', RegisterHandler)
 ], debug=True)
